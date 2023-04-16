4aaa16d382734657e27c60ba5e25f4c19b3ed86b
Thomas Carr <carrt313@gmail.com>
2023-04-15 20:10:15 -0400

    Added other Linkage Attack Attempts

--- Linkage Attack/SGN Based Linkage Attack/data.py
+++ Linkage Attack/SGN Based Linkage Attack/data.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset, DataLoader
 import os
 import torch
 import numpy as np
-# import h5py
+import h5py
 import random
 import os.path as osp
 import sys
@@ -27,197 +27,197 @@ class NTUDataset(Dataset):
         return len(self.y)
 
     def __getitem__(self, index):
-        return [self.x[index], int(self.y[index])]
+        return [self.x[index], self.y[index]]
 
 
-# class NTUDataLoaders(object):
-#     def __init__(self, dataset='NTU', case=0, aug=1, seg=30, train_X=None, train_Y=None, val_X=None, val_Y=None, test_X=None, test_Y=None):
-#         self.dataset = dataset
-#         self.case = case
-#         self.aug = aug
-#         self.seg = seg
-#         if train_X is None:
-#             self.create_datasets()
-#         else:
-#             self.train_X = train_X
-#             self.train_Y = train_Y
-#             self.val_X = val_X
-#             self.val_Y = val_Y
-#             self.test_X = test_X
-#             self.test_Y = test_Y
-#         self.train_set = NTUDataset(self.train_X, self.train_Y)
-#         self.val_set = NTUDataset(self.val_X, self.val_Y)
-#         self.test_set = NTUDataset(self.test_X, self.test_Y)
+class NTUDataLoaders(object):
+    def __init__(self, dataset='NTU', case=0, aug=1, seg=30, train_X=None, train_Y=None, val_X=None, val_Y=None, test_X=None, test_Y=None):
+        self.dataset = dataset
+        self.case = case
+        self.aug = aug
+        self.seg = seg
+        if train_X is None:
+            self.create_datasets()
+        else:
+            self.train_X = train_X
+            self.train_Y = train_Y
+            self.val_X = val_X
+            self.val_Y = val_Y
+            self.test_X = test_X
+            self.test_Y = test_Y
+        self.train_set = NTUDataset(self.train_X, self.train_Y)
+        self.val_set = NTUDataset(self.val_X, self.val_Y)
+        self.test_set = NTUDataset(self.test_X, self.test_Y)
 
-#     def get_train_loader(self, batch_size, num_workers):
-#         if self.aug == 0:
-#             return DataLoader(self.train_set, batch_size=batch_size, shuffle=True,
-#                               # num_workers=num_workers,
-#                               collate_fn=self.collate_fn_fix_val, drop_last=True)
-#         elif self.aug == 1:
-#             return DataLoader(self.train_set, batch_size=batch_size, shuffle=True,
-#                               # num_workers=num_workers,
-#                               collate_fn=self.collate_fn_fix_val, drop_last=True)
+    def get_train_loader(self, batch_size, num_workers):
+        if self.aug == 0:
+            return DataLoader(self.train_set, batch_size=batch_size, shuffle=True,
+                              # num_workers=num_workers,
+                              collate_fn=self.collate_fn_fix_val, drop_last=True)
+        elif self.aug == 1:
+            return DataLoader(self.train_set, batch_size=batch_size, shuffle=True,
+                              # num_workers=num_workers,
+                              collate_fn=self.collate_fn_fix_val, drop_last=True)
 
-#     def get_val_loader(self, batch_size, num_workers):
-#         if self.dataset == 'NTU' or self.dataset == 'kinetics' or self.dataset == 'NTU120':
-#             return DataLoader(self.val_set, batch_size=batch_size, shuffle=False,
-#                               # num_workers=num_workers,
-#                               collate_fn=self.collate_fn_fix_val, drop_last=True)
-#         else:
-#             return DataLoader(self.val_set, batch_size=batch_size, shuffle=False,
-#                               # num_workers=num_workers,
-#                               collate_fn=self.collate_fn_fix_val, drop_last=True)
+    def get_val_loader(self, batch_size, num_workers):
+        if self.dataset == 'NTU' or self.dataset == 'kinetics' or self.dataset == 'NTU120':
+            return DataLoader(self.val_set, batch_size=batch_size, shuffle=False,
+                              # num_workers=num_workers,
+                              collate_fn=self.collate_fn_fix_val, drop_last=True)
+        else:
+            return DataLoader(self.val_set, batch_size=batch_size, shuffle=False,
+                              # num_workers=num_workers,
+                              collate_fn=self.collate_fn_fix_val, drop_last=True)
 
-#     def get_test_loader(self, batch_size, num_workers):
-#         return DataLoader(self.test_set, batch_size=batch_size, shuffle=False,
-#                           # num_workers=num_workers,
-#                           collate_fn=self.collate_fn_fix_test, drop_last=True)
+    def get_test_loader(self, batch_size, num_workers):
+        return DataLoader(self.test_set, batch_size=batch_size, shuffle=False,
+                          # num_workers=num_workers,
+                          collate_fn=self.collate_fn_fix_test, drop_last=True)
 
-#     def get_train_size(self):
-#         return len(self.train_Y)
+    def get_train_size(self):
+        return len(self.train_Y)
 
-#     def get_val_size(self):
-#         return len(self.val_Y)
+    def get_val_size(self):
+        return len(self.val_Y)
 
-#     def get_test_size(self):
-#         return len(self.test_Y)
+    def get_test_size(self):
+        return len(self.test_Y)
 
-#     def create_datasets(self):
-#         if self.dataset == 'NTU':
-#             if self.case == 0:
-#                 self.metric = 'CS'
-#             elif self.case == 1:
-#                 self.metric = 'CV'
-#             path = osp.join('./data/ntu', 'NTU_' + self.metric + '.h5')
+    def create_datasets(self):
+        if self.dataset == 'NTU':
+            if self.case == 0:
+                self.metric = 'CS'
+            elif self.case == 1:
+                self.metric = 'CV'
+            path = osp.join('./data/ntu', 'NTU_' + self.metric + '.h5')
 
-#         f = h5py.File(path, 'r')
-#         self.train_X = f['x'][:]
-#         self.train_Y = np.argmax(f['y'][:], -1)
-#         self.val_X = f['valid_x'][:]
-#         self.val_Y = np.argmax(f['valid_y'][:], -1)
-#         self.test_X = f['test_x'][:]
-#         self.test_Y = np.argmax(f['test_y'][:], -1)
-#         f.close()
+        f = h5py.File(path, 'r')
+        self.train_X = f['x'][:]
+        self.train_Y = np.argmax(f['y'][:], -1)
+        self.val_X = f['valid_x'][:]
+        self.val_Y = np.argmax(f['valid_y'][:], -1)
+        self.test_X = f['test_x'][:]
+        self.test_Y = np.argmax(f['test_y'][:], -1)
+        f.close()
 
-#         # Combine the training data and validation data togehter as ST-GCN
-#         self.train_X = np.concatenate([self.train_X, self.val_X], axis=0)
-#         self.train_Y = np.concatenate([self.train_Y, self.val_Y], axis=0)
-#         self.val_X = self.test_X
-#         self.val_Y = self.test_Y
+        # Combine the training data and validation data togehter as ST-GCN
+        self.train_X = np.concatenate([self.train_X, self.val_X], axis=0)
+        self.train_Y = np.concatenate([self.train_Y, self.val_Y], axis=0)
+        self.val_X = self.test_X
+        self.val_Y = self.test_Y
 
-#     def collate_fn_fix_train(self, batch):
-#         """Puts each data field into a tensor with outer dimension batch size
-#         """
-#         x, y = zip(*batch)
+    def collate_fn_fix_train(self, batch):
+        """Puts each data field into a tensor with outer dimension batch size
+        """
+        x, y = zip(*batch)
 
-#         if self.dataset == 'kinetics' and self.machine == 'philly':
-#             x = np.array(x)
-#             x = x.reshape(x.shape[0], x.shape[1], -1)
-#             x = x.reshape(-1, x.shape[1] * x.shape[2], x.shape[3]*x.shape[4])
-#             x = list(x)
+        if self.dataset == 'kinetics' and self.machine == 'philly':
+            x = np.array(x)
+            x = x.reshape(x.shape[0], x.shape[1], -1)
+            x = x.reshape(-1, x.shape[1] * x.shape[2], x.shape[3]*x.shape[4])
+            x = list(x)
 
-#         x, y = self.Tolist_fix(x, y, train=1)
-#         lens = np.array([x_.shape[0] for x_ in x], dtype=np.int)
-#         # sort sequence by valid length in descending order
-#         idx = lens.argsort()[::-1]
-#         y = np.array(y)[idx]
-#         x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
+        x, y = self.Tolist_fix(x, y, train=1)
+        lens = np.array([x_.shape[0] for x_ in x], dtype=np.int)
+        # sort sequence by valid length in descending order
+        idx = lens.argsort()[::-1]
+        y = np.array(y)[idx]
+        x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
 
-#         if self.dataset == 'NTU':
-#             if self.case == 0:
-#                 theta = 0.3
-#             elif self.case == 1:
-#                 theta = 0.5
-#         elif self.dataset == 'NTU120':
-#             theta = 0.3
+        if self.dataset == 'NTU':
+            if self.case == 0:
+                theta = 0.3
+            elif self.case == 1:
+                theta = 0.5
+        elif self.dataset == 'NTU120':
+            theta = 0.3
 
-#         # data augmentation
-#         x = _transform(x, theta)
-#         # data augmentation
-#         y = torch.LongTensor(y)
+        # data augmentation
+        x = _transform(x, theta)
+        # data augmentation
+        y = torch.LongTensor(y)
 
-#         return [x, y]
+        return [x, y]
 
-#     def collate_fn_fix_val(self, batch):
-#         """Puts each data field into a tensor with outer dimension batch size
-#         """
-#         x, y = zip(*batch)
-#         x, y = self.Tolist_fix(x, y, train=1)
-#         idx = range(len(x))
-#         y = np.array(y)
+    def collate_fn_fix_val(self, batch):
+        """Puts each data field into a tensor with outer dimension batch size
+        """
+        x, y = zip(*batch)
+        x, y = self.Tolist_fix(x, y, train=1)
+        idx = range(len(x))
+        y = np.array(y)
 
-#         x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
-#         y = torch.LongTensor(y)
+        x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
+        y = torch.LongTensor(y)
 
-#         return [x, y]
+        return [x, y]
 
-    # def collate_fn_fix_test(self, batch):
-    #     """Puts each data field into a tensor with outer dimension batch size
-    #     """
-    #     x, y = zip(*batch)
-    #     x, labels = self.Tolist_fix(x, y, train=2)
-    #     idx = range(len(x))
-    #     y = np.array(y)
+    def collate_fn_fix_test(self, batch):
+        """Puts each data field into a tensor with outer dimension batch size
+        """
+        x, y = zip(*batch)
+        x, labels = self.Tolist_fix(x, y, train=2)
+        idx = range(len(x))
+        y = np.array(y)
 
-    #     x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
-    #     y = torch.LongTensor(y)
+        x = torch.stack([torch.from_numpy(x[i]) for i in idx], 0)
+        y = torch.LongTensor(y)
 
-    #     return [x, y]
+        return [x, y]
 
-    # def Tolist_fix(self, joints, y, train=1):
-    #     seqs = []
+    def Tolist_fix(self, joints, y, train=1):
+        seqs = []
 
-    #     for idx, seq in enumerate(joints):
-    #         zero_row = []
-    #         for i in range(len(seq)):
-    #             if (seq[i, :] == np.zeros((1, 150))).all():
-    #                 zero_row.append(i)
+        for idx, seq in enumerate(joints):
+            zero_row = []
+            for i in range(len(seq)):
+                if (seq[i, :] == np.zeros((1, 150))).all():
+                    zero_row.append(i)
 
-    #         seq = np.delete(seq, zero_row, axis=0)
+            seq = np.delete(seq, zero_row, axis=0)
 
-    #         seq = turn_two_to_one(seq)
-    #         seqs = self.sub_seq(seqs, seq, train=train)
+            seq = turn_two_to_one(seq)
+            seqs = self.sub_seq(seqs, seq, train=train)
 
-    #     return seqs, y
+        return seqs, y
 
-    # def sub_seq(self, seqs, seq, train=1):
-    #     group = self.seg
-    #     if self.dataset == 'SYSU' or self.dataset == 'SYSU_same':
-    #         seq = seq[::2, :]
+    def sub_seq(self, seqs, seq, train=1):
+        group = self.seg
+        if self.dataset == 'SYSU' or self.dataset == 'SYSU_same':
+            seq = seq[::2, :]
 
-    #     if seq.shape[0] < self.seg:
-    #         pad = np.zeros(
-    #             (self.seg - seq.shape[0], seq.shape[1])).astype(np.float32)
-    #         seq = np.concatenate([seq, pad], axis=0)
+        if seq.shape[0] < self.seg:
+            pad = np.zeros(
+                (self.seg - seq.shape[0], seq.shape[1])).astype(np.float32)
+            seq = np.concatenate([seq, pad], axis=0)
 
-    #     ave_duration = seq.shape[0] // group
+        ave_duration = seq.shape[0] // group
 
-    #     if train == 1:
-    #         offsets = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
-    #         seq = seq[offsets]
-    #         seqs.append(seq)
+        if train == 1:
+            offsets = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+            seq = seq[offsets]
+            seqs.append(seq)
 
-    #     elif train == 2:
-    #         offsets1 = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
-    #         offsets2 = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
-    #         offsets3 = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
-    #         offsets4 = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
-    #         offsets5 = np.multiply(
-    #             list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+        elif train == 2:
+            offsets1 = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+            offsets2 = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+            offsets3 = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+            offsets4 = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
+            offsets5 = np.multiply(
+                list(range(group)), ave_duration) + np.random.randint(ave_duration, size=group)
 
-    #         seqs.append(seq[offsets1])
-    #         seqs.append(seq[offsets2])
-    #         seqs.append(seq[offsets3])
-    #         seqs.append(seq[offsets4])
-    #         seqs.append(seq[offsets5])
+            seqs.append(seq[offsets1])
+            seqs.append(seq[offsets2])
+            seqs.append(seq[offsets3])
+            seqs.append(seq[offsets4])
+            seqs.append(seq[offsets5])
 
-    #     return seqs
+        return seqs
 
 
 class AverageMeter(object):
@@ -290,10 +290,10 @@ def _transform(x, theta):
     return x
 
 
-# if __name__ == '__main__':
-#     path = osp.join('./data/ntu', 'NTU_' + "CS" + '.h5')
-#     f = h5py.File(path, 'r')
-#     print(f['x'])
-#     print(f['y'])
-#     print(f['test_y'])
-#     print(f['valid_y'])
+if __name__ == '__main__':
+    path = osp.join('./data/ntu', 'NTU_' + "CS" + '.h5')
+    f = h5py.File(path, 'r')
+    print(f['x'])
+    print(f['y'])
+    print(f['test_y'])
+    print(f['valid_y'])
